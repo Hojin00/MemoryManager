cf8fc43b5c1d75ac12d55c2b896353817c441305
lccxm <luccamolon@gmail.com>
2022-06-07 09:20:18 -0300

    fileReader v1 and Process

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,1 +1,31 @@
-// lalalal
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+
+public class FileReader {
+  
+    void read(string filename) {
+        try {
+            File myObj = new File("test.txt");
+            Scanner scanner = new Scanner(myObj);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                String[] parts = data.split("(");
+                String[] values = parts[1].split(",");
+                values[1].replaceAll(".$", "");
+                if(parts[0].equals("IN")) {
+                    Process p = new Process(values[0], values[1]);
+                } else {
+                    //implementar :)
+                }
+                System.out.println(data);
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    }
+
+  
+}
---
+++ src/Process.java
@@ -0,0 +1,18 @@
+public class Process {
+    private String name;
+    private int size;
+
+
+    Process(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getSize() {
+        return size;
+    }
+}
