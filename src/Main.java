cd1d33f5e7376666b7f275db1584f84f6e8475b4
Hojin00 <robert.ryu01@gmail.com>
2022-06-07 09:28:19 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@
 	// particoes variaveis
 		// informar a política de alocação
 			// 1. Best-fit 2. Worst-fit
+// requisicao de alocacao
+	// comando "IN", com nome do processo "ID", e tam do proc "tamNP"
+// requisicao de liberacao
+	// comando "OUT", com nome do processo "ID"
+	
 
