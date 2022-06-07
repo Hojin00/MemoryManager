582417aba6f3e46564465498d7b7ad81a30beec5
Hojin Ryu <19200169@facin.portoalegre.pucrsnet.br>
2022-06-07 08:45:32 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,7 @@
-sdf
+//alocacao de espaco contiguo
+	// particoes fixas de tamanho
+		// informar o tamanho da partição
+	// particoes variaveis
+		// informar a política de alocação
+			// 1. Best-fit 2. Worst-fit
+
