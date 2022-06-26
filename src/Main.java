331b9001e95e49b9d45de4d35db5ee0d392cf4c4
Hojin00 <63571876+Hojin00@users.noreply.github.com>
2022-06-26 14:28:35 -0300

    Manager sized error fixed & enunciado

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 //alocacao de espaco contiguo
-	// particoes fixas de tamanho
-		// informar o tamanho da partição
-	// particoes variaveis
-		// informar a política de alocação
-			// 1. Best-fit 2. Worst-fit
+// particoes fixas de tamanho
+// informar o tamanho da partição
+// particoes variaveis
+// informar a política de alocação
+// 1. Best-fit 2. Worst-fit
 // requisicao de alocacao
-	// comando "IN", com nome do processo "ID", e tam do proc "tamNP"
+// comando "IN", com nome do processo "ID", e tam do proc "tamNP"
 // requisicao de liberacao
-	// comando "OUT", com nome do processo "ID"
-	
+// comando "OUT", com nome do processo "ID"
+
 // import java.util.ArrayList;
+public class Main {
+	public static void main(String[] args) {
 
-public static void main(String[] args) {
+		Process[] manager = MemoryManager(3);
+		FileReader reader = new FileReader();
 
-	Process[] manager = MemoryManager(3);
-	FileReader reader = new FileReader();
-
-	// reader.read("test2.txt", manager); nao testado
+		// reader.read("test2.txt", manager); nao testado
+	}
 }
--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -5,10 +5,10 @@ public class MemoryManager {
     private ArrayList<Process> memory;
     private int blocksize;
     private boolean hasFixedSize = true;
-    
-    public MemoryManager(double size) {
-        size = Math.pow(2, size);
-        this.memory = new ArrayList<Process>((int)(size));
+
+    public MemoryManager(int size) {
+        size = (int) Math.pow(2, size);
+        this.memory = new ArrayList<Process>((int) (size));
     }
 
     public ArrayList<Process> getMemoryInfo() {
@@ -18,14 +18,14 @@ public class MemoryManager {
     public Process getMemoryInfoById(int id) {
         return memory.get(id);
     }
-    
+
     public void addProcess(Process p) {
         memory.add(p);
     }
 
     public void removeProcess(String processName) {
-        for(int i = 0; i < memory.size(); i++) {
-            if(memory.get(i).getName().equals(processName)) {
+        for (int i = 0; i < memory.size(); i++) {
+            if (memory.get(i).getName().equals(processName)) {
                 Process p = memory.get(i);
                 memory.remove(p);
                 break;
